bec56d9388aa5c9102e965b9dc5d1173b8f53bf7
Pengyonaut <81331432+Pengyonaut@users.noreply.github.com>
2022-11-07 07:54:25 +0100

    update flags vercel

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import Flags from './components/Flags';
+import Header from "./components/Header"
+import TitleSection from './components/TitleSection';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./assets/theme.scss"
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Header/>
+
+    <Flags/>
+
     </div>
   );
 }
